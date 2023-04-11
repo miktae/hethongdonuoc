928a5bababb778eb5c7cc1bd06f242a5b6b1ce88
nguyenvanthanh221101 <86827133+nguyenvanthanh221101@users.noreply.github.com>
2023-04-11 11:04:32 +0700

    title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,15 @@ import "./navbar.css";
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <span className="navbar-brand">
-          <a href="./" className="navbar-link">LORA Web</a>
+        <div>
+          <span className="navbar-brand">
+          <a href="./" className="navbar-link">PHẦN MỀM QUẢN LÝ ĐỒNG HỒ NƯỚC ĐIỆN TỬ </a>
         </span>
+        <p>
+          Tên đề tài/ đề án: "Nghiên cứu, thiết kế và chế tạo đồng hồ 
+          đo lưu lượng nước điện tử"
+        </p>
+        </div>
       </nav>
     );
   };
